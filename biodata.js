789c17e3a48ce3a6db3b8ce5749c50b822638a6e
nurudeen sodeinde <nurudeensodeinde@gmail.com>
2022-06-24 19:03:34 +0100

    first commit

---
+++ biodata.js
@@ -0,0 +1,9 @@
+let fullName="NURUDEEN SODEINDE";
+var height="5ft";
+var country="NIGERIA";
+
+console.log(fullName);
+console.log(height);
+console.log(country);
+
+document.write("NURUDEEN SODEINDE"+"<br>"+"5ft"+"<br>"+"NIGERIA");
